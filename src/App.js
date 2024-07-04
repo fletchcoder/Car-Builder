28d816487963c0744f8eee6b4fa8f4bbb775a08d
fletchcoder <131314453+fletchcoder@users.noreply.github.com>
2024-07-04 10:39:01 -0700

    Added images to exported const and imported 3D Canvas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
 import "./styles/App.css";
 
 function App() {
-	return <div className="App">Canvas</div>;
+	return (
+		<>
+			<div className="App">Canvas</div>
+			<Canvas>
+				<Sky></Sky>
+			</Canvas>
+		</>
+	);
 }
 
 export default App;
---
+++ src/js/images.js
@@ -0,0 +1,47 @@
+import blackWool from "images/colors/black_wool.png";
+import blueWool from "images/colors/blue_wool.png";
+import brownWool from "images/colors/brown_wool.png";
+import cyanWool from "images/colors/cyan_wool.png";
+import grayWool from "images/colors/gray_wool.png";
+import greenWool from "images/colors/green_wool.png";
+import lightBlueWool from "images/colors/light_blue_wool.png";
+import lightGrayWool from "images/colors/light_gray_wool.png";
+import limeWool from "images/colors/lime_wool.png";
+import magentaWool from "images/colors/magenta_wool.png";
+import orangeWool from "images/colors/orange_wool.png";
+import pinkWool from "images/colors/pink_wool.png";
+import redWool from "images/colors/red_wool.png";
+import whiteWool from "images/colors/white_wool.png";
+import yellowWool from "images/colors/yellow_wool.png";
+import blackGlass from "images/glass/black_stained_glass.png";
+import cyanGlass from "images/glass/cyan_stained_glass.png";
+import blueGlass from "images/glass/light_blue_stained_glass.png";
+import grayGlass from "images/glass/light_gray_stained_glass.png";
+import blockWheel from "images/wheels/chiseled_quartz_block.png";
+import spiralWheel from "images/wheels/cobweb.png";
+
+const images = {
+	blackWool,
+	blueWool,
+	brownWool,
+	cyanWool,
+	grayWool,
+	greenWool,
+	lightBlueWool,
+	lightGrayWool,
+	limeWool,
+	magentaWool,
+	orangeWool,
+	pinkWool,
+	redWool,
+	whiteWool,
+	yellowWool,
+	blackGlass,
+	cyanGlass,
+	blueGlass,
+	grayGlass,
+	blockWheel,
+	spiralWheel,
+};
+
+export default images;
